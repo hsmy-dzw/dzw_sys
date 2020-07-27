a6ce36e0e3d982dd479d1ab557171a55300ea53a
1661719505 <1661719505@qq.com>
2020-07-27 10:08:06 +0800

    213123123123123

---
+++ src/main/java/org/accp/dao/wc/khclbMapper.java
@@ -0,0 +1,22 @@
+package org.accp.dao.wc;
+
+import java.util.List;
+
+import org.accp.pojo.khclb;
+import org.apache.ibatis.annotations.Param;
+
+public interface khclbMapper {
+    int deleteByPrimaryKey(Integer cid);
+
+    int insert(khclb record);
+
+    int insertSelective(khclb record);
+
+    khclb selectByPrimaryKey(Integer cid);
+
+    int updateByPrimaryKeySelective(khclb record);
+
+    int updateByPrimaryKey(khclb record);
+    
+    public List<khclb> qurechclByid(@Param("kid") Integer kid);
+}
